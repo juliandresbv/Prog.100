d1a425cebac12bc047858856a13186a1f1f6afbb
unknown <VEQ0108@ad.uniandes.edu.co>
2018-09-19 12:17:45 -0500

    Nombre del archivo a crear modificado

--- S7_C1 (Archivos)/Archivos.m
+++ S7_C1 (Archivos)/Archivos.m
@@ -1,7 +1,7 @@
 %%   Creacion de archivo binario
 
 buffer_w = round(rand(1, 1000)*100);        %   Se CREAN 1000 datos de tipo double entre 0 - 100
-arch_w = fopen('thousand.bin', 'w');        %   Se crea un MANEJADOR de archivo (arch_w).
+arch_w = fopen('arch.bin', 'w');        %   Se crea un MANEJADOR de archivo (arch_w).
                                             %   creando un archivo con nombre "thosand.bin"
 fwrite(arch_w, int8(buffer_w), 'int8');     %   Se crea el archivo guardando cada valor de buffer
                                             %   en format int8, cada valor ocupa 8 BITS (1 BYTE).
