e558b89d16f57dfd45b65cd804ab05989dc7292a
unknown <VEQ0105@ad.uniandes.edu.co>
2018-09-26 12:20:01 -0500

    S8_C1: GaussJordan. Correcion de bintodec

---
+++ ClasesMagistrales/S8_C1 (GaussJordan)/GaussJordan.m
@@ -0,0 +1,52 @@
+function [Au] = GaussJordan(A, B)
+    %   GAUSSJORDAN. Funcion que calcula la matriz de GaussJordan para una
+    %   matriz A(NxN) con soluciones en la matriz B(NxM).
+    
+    %   I. Crear matriz aumentada Au = [A B eye(size(A, 1))]
+    %       Alojar espacio en memmoria.
+    Au = zeros(size(A, 1), 2*size(A, 2) + size(B, 2));
+    
+    %       Llenar matriz acorde a Au = [A B eye(size(A, 1))]
+    Au = [A B eye(size(A, 1))];
+    
+    %   II. Proceso iterativo por filas.
+    %       Escoger un pivote y convertirlo en 1 (Multiplicar la fila por 
+    %       el inverso multiplicativo del pivote).
+    for row_count = 1:size(A, 1)
+        %   En este caso, se asume que los valorese en la diagonal no son
+        %   0.
+        
+        %   Se selecciona el pivote.
+        pivote = Au(row_count, row_count);
+        
+        %   Se verifica que el pivote no sea 0.
+        %   En caso de que sea 0, se debe intercambiar la fila actual por
+        %   la fila con el mayor valor en la columna.
+        %   TO-DO
+        if (pivote == 0)
+            
+        end
+        
+        %   II.I. Se convierte el pivote en 1.
+        Au(row_count, :) = Au(row_count, :).*(1/pivote);
+        
+        %   Aqui en el metodo de Gauss el indice inner_row_count puede 
+        %   tomar el valor de inner_row_count = row_count + 1.
+        %   Para GaussJordan, el indice va de 1 a size(A, 1), ya que revisa
+        %   todas las filas.
+        for inner_row_count = 1:size(A, 1)
+            if (inner_row_count == row_count)
+                continue;
+            end
+            
+            %   II.II Se convierte en 0 los elementos de la columna del
+            %   pivote.
+            Au(inner_row_count, :) = Au(inner_row_count, :) + (-Au(inner_row_count, row_count)).*(Au(row_count, :));
+        end
+    end
+end
+
+%   Test cases:
+%   A = [4 3 2;-1 -1/2 4;-3/4 4 25]
+%   B = [5 7;3 1;13 2]
+
--- Tareas&Labs/T1 (BinDecFloatIEEE754)/bintodec.m
+++ Tareas&Labs/T1 (BinDecFloatIEEE754)/bintodec.m
@@ -2,11 +2,11 @@
 function [d] = bintodec(b)
     % BINTODEC
     % Esta funcion convierte un numero binario a decimal.
-    % Formato de entrada: arreglo de numeros binarios ([(0|1) ... (0|1)]).
+    % Formato de entrada: arreglo de numeros binarios ([(0|1) (0|1) ... (0|1) (0|1)]).
     d = 0;
     for i = length(b):-1:1
         if(b(i) == 1)
-            d = d + (2^(i-1));
+            d = d + (2^(length(b) - i));
         end
     end
 end
