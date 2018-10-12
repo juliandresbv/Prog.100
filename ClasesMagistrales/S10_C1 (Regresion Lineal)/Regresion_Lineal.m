fa5edb2b14abb80b635841d565d023f431a4cab5
unknown <VEQ0214@ad.uniandes.edu.co>
2018-10-12 11:40:14 -0500

    Coeficiente de determinacion, Calculos de valores con funciones de MATLAB

--- ClasesMagistrales/S10_C1 (Regresion Lineal)/Regresion_Lineal.m
+++ ClasesMagistrales/S10_C1 (Regresion Lineal)/Regresion_Lineal.m
@@ -1,3 +1,5 @@
+format long g;
+
 %   Leer datos guardados en double
 arch_xi = fopen('XDatos.bin', 'r');
 Xi = fread(arch_xi, 'double');
@@ -27,7 +29,34 @@ a_0 = Y_media - (a_1  * X_media);
 
 Yi_hat = (a_1 * Xi) + a_0;
 
+%   Grafica.
+
 figure;
 plot(Xi, Yi, '.');
 hold on;
 plot(Xi, Yi_hat, 'r');
+
+%   Valores de a_0, a_1, Yi_hat determinados por MATLAB.
+p = polyfit(Xi, Yi, 1);
+Yi_hat_MATLAB = polyval(p, Xi);
+
+%   Calcular coeficiente de determinacion.
+Yi_minus_Yi_hat = zeros(length(Yi), 1);
+
+for i = 1:length(Yi)
+    Yi_minus_Yi_hat(i) = ( Yi(i) - Yi_hat(i) ).^2;
+end
+
+Yi_minus_Y_media = zeros(length(Yi), 1);
+
+for j = 1:length(Yi)
+    Yi_minus_Y_media(j) = ( Yi(j) - Y_media ).^2;
+end
+
+sum_Yi_minus_Yi_hat = sum( Yi_minus_Yi_hat );
+sum_Yi_minus_Y_media = sum( Yi_minus_Y_media );
+
+%   Coeficiente de determinacion. Valor entre el intervao (0,1).
+%   Si es cercano a cero (0), la regresion lineal no es buena.
+%   Si es cercano a cero (0), la regresion lineal es buena.
+r_sq = 1 - ( sum_Yi_minus_Yi_hat/sum_Yi_minus_Y_media );
